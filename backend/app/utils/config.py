24b6273dda2d9b89a94557373c1984fb1ae27c8b
Atharva Deore <140725599+adeore09@users.noreply.github.com>
2026-06-25 04:31:54 +0530

    completed with intial UI for auth and dashboard

---
+++ backend/app/utils/config.py
@@ -0,0 +1,21 @@
+import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "PortX Engine"
+    API_V1_STR: str = "/api/v1"
+    
+    POSTGRES_SERVER: str="localhost"
+    POSTGRES_USER: str="postgres"
+    POSTGRES_PASSWORD: str = "postgres"
+    POSTGRES_DB: str = "portx"
+    DATABASE_URL: str | None = None
+    
+    model_config = SettingsConfigDict(case_sensitive=True, env_file=".env")
+    
+    def __init__(self, **values):
+        super().__init__(**values))
+        if not self.DATABASE_URL:
+            self.DATABASE_URL = f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_SERVER}:5432/{self.POSTGRES_DB}"
+
+settings = Settings()
